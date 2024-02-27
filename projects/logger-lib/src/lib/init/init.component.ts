cd7783626576eadb6abff081543753783f4f5f3d
Gregor Doroschenko <g.doroschenko@gmail.com>
2024-02-27 09:22:28 +0100

    Add app and lib

---
+++ projects/logger-lib/src/lib/init/init.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-init',
+  standalone: true,
+  imports: [],
+  templateUrl: './init.component.html',
+  styleUrl: './init.component.css'
+})
+export class InitComponent {
+
+}
---
+++ projects/logger-lib/src/lib/logger-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-logger-lib',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      logger-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class LoggerLibComponent {
+
+}
